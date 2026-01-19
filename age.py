c4defe996bc73fb821dd50649a0a2c27efc7294e
marlu-ai <marielumieremakoudoum@gmail.com>
2026-01-19 13:19:01 +0100

    Add files via upload
    Day 1 - Python basics

---
+++ age.py
@@ -0,0 +1,5 @@
+age=int(input("Quel age avez-vous ? "))
+if age<18:
+    print("Acces non autorise. Vous devriez avoir plus de 18 ans.")
+else:
+    print("Acces autorise. Bienvenue!")
---
+++ conversion.py
@@ -0,0 +1,3 @@
+montant=input("Entrez le montant en franc CFA  à convertir en dollars : ")
+montant_dollars=float(montant)/650
+print("Le montant en dollars est de : " + str(montant_dollars) + " $")
